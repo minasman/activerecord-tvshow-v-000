0974d3f33e83aaab2eb1b4592f4384c3bfb432e2
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-06-30 16:35:10 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -7,4 +7,19 @@ class Show < ActiveRecord::Base
   def self.most_popular_show 
     self.find_by(rating: self.highest_rating)
   end
+  
+  def lowest_rating 
+    self.minimum("rating")
+  end
+  
+  def least_popular_show
+    self.find_by(rating: self.lowest_rating)
+  end
+  
+  def ratings_sum 
+    self.sum("rating")
+  end 
+  
+  def popular_shows 
+    self.where(rating)
 end
